b1a5a1c765bc366be20a8454a095ccd4ea5fa7b3
M7mdddibrahim <m7mdalhussini@gmail.com>
2023-10-23 14:44:12 +0300

    load nosnos

--- MyWindow.py
+++ MyWindow.py
@@ -38,3 +38,28 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.verticalLayout_8.addWidget(self.graphWidget1)
         self.ui.verticalLayout_6.addWidget(self.graphWidget2)
         self.ui.verticalLayout_7.addWidget(self.graphWidget3)
+        self.actionLoad.triggered.connect(self.Load)
+
+        def Load(self):
+            filename = QtWidgets.QFileDialog.getOpenFileName()
+            path = filename[0]
+            if path.endswith(ext):
+                if path.endswith(".txt"):
+                    with open(path, "r") as data:
+                        x = []
+                        y = []
+                        for line in data:
+                            p = line.split()
+                            x.append(float(p[0]))
+                            y.append(float(p[1]))
+                    newplot = PlotLine()
+                    newplot.data = pd.DataFrame({"time": x, "amplitude": y})
+                    PlotLines1.append(newplot)
+                    newplot.ChannelNum = 1
+                else:
+                    newplot = PlotLine()
+                    newplot.data = pd.read_csv(path, usecols=["time", "amplitude"])
+                    PlotLines1.append(newplot)
+                    newplot.ChannelNum = 1
+            else:
+                self.ErrorMsg("You can only load .txt or .csv files.")
